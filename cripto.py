87e518305a6337a1acffa6ee2c59f5b1d1161795
Brennosg <brennooliveira3636@gmail.com>
2024-07-17 00:00:17 -0300

    first commit

---
+++ cripto.py
@@ -0,0 +1,28 @@
+from cryptography.fernet import Fernet
+
+#key = Fernet.generate_key()
+
+#with open('mykey.key', 'wb') as mykey:
+    #mykey.write(key)
+    
+with open('mykey.key', 'rb') as mykey:
+    key = mykey.read()
+    
+##    key = mykey.read()
+    
+####    original = original_file.read()
+    
+#encrypted = f.encrypt(original)
+
+#with open('enc_grades.csv', 'wb') as encrypted_file:
+#    encrypted_file.write(encrypted)
+
+f = Fernet(key)    
+
+with open('enc_grades.csv', 'rb') as encrypted_file:
+    encrypted = encrypted_file.read()
+
+decrypted = f.decrypt(encrypted)
+
+with open('dec_grades.csv', 'wb') as decrypted_file:
+    decrypted_file.write(decrypted)
